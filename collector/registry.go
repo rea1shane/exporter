74bbb465d547e41fb2d092f68dfc8f578747adc6
Shane <develop@shenyingnan.com>
2024-12-09 14:16:18 +0800

    update desc

--- collector/registry.go
+++ collector/registry.go
@@ -14,8 +14,8 @@ const (
 
 var (
 	factories        = make(map[string]func(namespace string, logger *logrus.Entry) (Collector, error)) // factories records all collector's construction method
-	collectorState   = make(map[string]*bool)                                                           // collectorState records all collector's default state (enable or disable)
-	forcedCollectors = map[string]bool{}                                                                // forcedCollectors collectors which have been explicitly enabled or disabled
+	collectorState   = make(map[string]*bool)                                                           // collectorState records all collector's default state (enabled or disabled)
+	forcedCollectors = map[string]bool{}                                                                // forcedCollectors will record collectors that have explicitly declared state
 )
 
 func RegisterCollector(collector string, isDefaultEnabled bool, factory func(namespace string, logger *logrus.Entry) (Collector, error)) {
