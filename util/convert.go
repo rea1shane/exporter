8783af86ce2fabd6a615735f0f50c0a9dec9b19f
Shane <develop@shenyingnan.com>
2024-12-10 11:14:59 +0800

    update any to float64

--- util/convert.go
+++ util/convert.go
@@ -2,49 +2,50 @@ package util
 
 import (
 	"fmt"
+	"math"
+	"reflect"
+	"strconv"
+)
+
+// https://stackoverflow.com/questions/20767724/converting-unknown-interface-to-float64-in-golang
+
+var (
+	floatType  = reflect.TypeOf(float64(0))
+	stringType = reflect.TypeOf("")
 )
 
 func AnyToFloat64(data any) (float64, error) {
-	var f float64
 	switch d := data.(type) {
-	case uint8:
-		f = float64(d)
-	case uint16:
-		f = float64(d)
-	case uint32:
-		f = float64(d)
-	case uint64:
-		f = float64(d)
+	case float64:
+		return d, nil
+	case float32:
+		return float64(d), nil
 	case int64:
-		f = float64(d)
-	case *uint8:
-		if d == nil {
-			return 0, fmt.Errorf("can't convert %v to float64", data)
-		}
-		f = float64(*d)
-	case *uint16:
-		if d == nil {
-			return 0, fmt.Errorf("can't convert %v to float64", data)
-		}
-		f = float64(*d)
-	case *uint32:
-		if d == nil {
-			return 0, fmt.Errorf("can't convert %v to float64", data)
-		}
-		f = float64(*d)
-	case *uint64:
-		if d == nil {
-			return 0, fmt.Errorf("can't convert %v to float64", data)
-		}
-		f = float64(*d)
-	case *int64:
-		if d == nil {
-			return 0, fmt.Errorf("can't convert %v to float64", data)
-		}
-		f = float64(*d)
+		return float64(d), nil
+	case int32:
+		return float64(d), nil
+	case int:
+		return float64(d), nil
+	case uint64:
+		return float64(d), nil
+	case uint32:
+		return float64(d), nil
+	case uint:
+		return float64(d), nil
+	case string:
+		return strconv.ParseFloat(d, 64)
 	default:
-		return 0, fmt.Errorf("can't convert %v to float64", data)
+		v := reflect.ValueOf(data)
+		v = reflect.Indirect(v)
+		if v.Type().ConvertibleTo(floatType) {
+			fv := v.Convert(floatType)
+			return fv.Float(), nil
+		} else if v.Type().ConvertibleTo(stringType) {
+			sv := v.Convert(stringType)
+			s := sv.String()
+			return strconv.ParseFloat(s, 64)
+		} else {
+			return math.NaN(), fmt.Errorf("can't convert %v to float64", v.Type())
+		}
 	}
-
-	return f, nil
 }
